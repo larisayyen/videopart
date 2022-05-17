20d6365af13cbf95ab98d23fc4ec0da20b3e69f8
yanyi951014 <10889949+yanyi951014@user.noreply.gitee.com>
2022-05-17 20:05:45 +0800

    fast updated

--- api/fast.py
+++ api/fast.py
@@ -63,5 +63,35 @@ async def pose_v(file:UploadFile = File(...)):
         f.write(contents)
 
     vid = cv2.VideoCapture(file.filename)
+    _, frame = vid.read()
 
-    return StreamingResponse(squat_analysis(vid))
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+
+        results = holistic.process(frame)
+
+        pose = results.pose_landmarks.landmark
+        pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+
+        x_pred = pd.DataFrame(pose_row)
+        x_pred = x_pred.T.to_numpy()
+
+        model = pickle.load(open(local_model,"rb"))
+        y_pred = model.predict(x_pred[0].reshape(1,132))
+
+    if y_pred == 'squat':
+        out_put = squat_analysis(vid)
+        return StreamingResponse(io.BytesIO(out_put),media_type='video/mp4')
+    elif y_pred == 'deadlift':
+        out_put = deadlift_analysis(vid)
+        return StreamingResponse(io.BytesIO(out_put),media_type='video/mp4')
+    elif y_pred == 'bench':
+        out_put = bench_analysis(vid)
+        return StreamingResponse(io.BytesIO(out_put),media_type='video/mp4')
+    elif y_pred == 'pushup':
+        out_put = pushup_analysis(vid)
+        return StreamingResponse(io.BytesIO(out_put),media_type='video/mp4')
+    elif y_pred == 'bridge':
+        out_put = bridge_analysis(vid)
+        return StreamingResponse(io.BytesIO(out_put),media_type='video/mp4')
+    else:
+        return "Fail to read the video."
