13d1371bbe390bfd0275760b3a0c6cc0961f7cc9
yanyi951014 <10889949+yanyi951014@user.noreply.gitee.com>
2022-05-24 20:18:32 +0800

    connection error unsolved

--- app.py
+++ app.py
@@ -18,22 +18,21 @@ footer {visibility: hidden;}
 
 # API call references
 base_url = 'replace with new url'
-local_url = 'http://localhost:8000'
+local_url = 'http://127.0.0.1:8000'
 
 # app title and description
 st.header('AI Workout Assistant')
 
 # user upload
-video_file_buffer = st.file_uploader('Choose a file')
+video_file_buffer = st.file_uploader('Choose a file', accept_multiple_files=True)
 
-if video_file_buffer is not None:
-    # video_file = open(video_file_buffer.filename,'rb')
-    video_bytes = video_file_buffer.read()
-    st.video(video_bytes)
+if video_file_buffer:
+    video_bytes = video_file_buffer[0].getvalue()
+
     request_url = f"{local_url}/pose_video"
-    if requests.post(request_url, files={'file': video_bytes}).status_code == 200:
-        with st.spinner('Wait for it...'):
-            response = requests.post(request_url, files={'file': video_bytes})
-            st.write('Please check output video.')
-    else:
-        st.write('Please try again.')
+    # if requests.post(request_url, files={'file': video_bytes}).status_code == 200:
+    with st.spinner('Wait for it...'):
+        response = requests.post(request_url, files={'file': video_bytes})
+        st.write('Please check output video.')
+# else:
+#     st.write('Please try again.')
